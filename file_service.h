bf48a2cdbfab33c23541bb30a82fc6be5b19dc34
JacobK1337 <jakub.kimsza@gmail.com>
2022-04-27 21:09:56 +0200

    initial file service functionality

---
+++ file_service.h
@@ -0,0 +1,33 @@
+#ifndef SOPROJECT_FILE_SERVICE_H
+#define SOPROJECT_FILE_SERVICE_H
+
+mode_t getMode(char *path);
+
+time_t getDateOfModify(char *path);
+
+void setDateOfModify(char *path, time_t newDate);
+
+void setMode(char *path, mode_t newMode);
+
+int isDirectory(char *path);
+
+int existsInDirectory(DIR *directory, char *fileName)
+
+char *appendToPath(char *path, char *attach);
+
+void copyFile(char* src, char* dest, int size);
+
+off_t getFileSize(char* path);
+
+void removeFile(char *path)
+
+void removeDirectory(char *path);
+
+void startDaemon();
+
+void sync(DIR* src, DIR* dest)
+
+void signalHandler(int signal);
+
+
+#endif //SOPROJECT_FILE_SERVICE_H
